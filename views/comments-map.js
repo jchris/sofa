ac7f5776a1f07283db622b9bef60cf5750ae99f8
Chris Anderson <jchris@grabb.it>
2008-10-18 13:39:38 -0700

    fixed up for relative paths

--- views/comments-map.js
+++ views/comments-map.js
@@ -1,3 +1,4 @@
 function(doc) {
-  if (doc.date && doc.comment && doc.postid) emit([doc.postid, doc.date], doc);
+  if (doc.date && doc.comment && doc.postid) 
+    emit([doc.postid, doc.date], doc);
 };
