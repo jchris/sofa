26c90eb4fdcadf60c6d678f65bc8dbe4b34bbdd9
Chris Anderson <jchris@grabb.it>
2008-12-10 09:17:37 -0800

    convert to new posts format

--- views/recent-posts-map.js
+++ views/recent-posts-map.js
@@ -1,7 +1,7 @@
 function(doc) {
   // only posts have slugs, dates, and html
   if (doc.type == "post") {
-    var summary = (doc.html.substring(0,350) + '...').replace(/<(.|\n)*?>/g, '');
+    var summary = (doc.html.replace(/<(.|\n)*?>/g, '').substring(0,350) + '...');
     emit(doc.created_at, {
       summary : summary,
       title : doc.title,
