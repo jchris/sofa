ff17e34cfbfefce99ff17068a220ef3744adc934
Chris Anderson <jchris@grabb.it>
2009-01-01 23:33:00 -0800

    comments work again

--- views/comments/map.js
+++ views/comments/map.js
@@ -2,8 +2,7 @@ function(doc) {
   // include-lib
   if (doc.type == "comment") {
     eval(lib.helpers.md5);
-  }
-  
-  doc.commenter.gravatar = hex_md5(doc.commenter.email);
-  emit([doc.post_id, doc.created_at], doc);
+    doc.commenter.gravatar = hex_md5(doc.commenter.email);
+    emit([doc.post_id, doc.created_at], doc);
+  }  
 };
