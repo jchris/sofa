f7e0778cc3cfb6909f5fa64ca7ea86c2c6319555
Chris Anderson <jchris@grabb.it>
2009-01-23 21:27:16 -0800

    simplify post page rendering

--- shows/post.js
+++ shows/post.js
@@ -1,27 +1,13 @@
 function(doc, req) {  
   // !json lib.templates
   // !code lib.helpers.template
-
-  var postHtml = doc.html.replace(/<script(.|\n)*?>/g, '');
-
-  return respondWith(req, {
-    html : function() {
-
-      var html = template(lib.templates.post, {
-        title : doc.title,
-        post : postHtml,
-        date : doc.created_at,
-        author : doc.author,
-        attachments : ['',req.path[0], '_design', req.path[2]].join('/')
-      });
-      return { body: html };
-    },
-    xml : function() {
-      return {
-        body: template('post.e4x', {
-          post : postHtml
-      })};
-    },
-    fallback : 'html'
-  })
+  
+  // we only show html
+  return template(lib.templates.post, {
+    title : doc.title,
+    post : doc.html,
+    date : doc.created_at,
+    author : doc.author,
+    attachments : ['',req.path[0], '_design', req.path[2]].join('/')
+  });
 }
