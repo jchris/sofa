943ec795f863ba7a40a12ee700d5edd2ca772c83
Chris Anderson <jchris@grabb.it>
2009-01-27 15:08:48 -0800

    you should upgrade couch

--- shows/edit.js
+++ shows/edit.js
@@ -4,13 +4,11 @@ function(doc, req) {
   // !code lib.helpers.template
   // !code lib.helpers.couchapp
 
-  // log("EDIT HELLO");
-  // var docid = doc ? doc._id : null;
-  // log("docid":docid);
   // we only show html
   return template(lib.templates.edit, {
     doc : doc,
-    docid : toJSON((doc && doc._id) || null),
+    // couch trunk newer than r738295 required for toJSON()
+    docid : toJSON((doc && doc._id) || null), 
     blog : blog,
     assets : assetPath(),
     index : listPath('index/recent-posts?descending=true')
