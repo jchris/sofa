6e35e15bc077f88b7558e940a2c554b1f59fc592
Jan Lehnardt <jan@dahlia.(none)>
2009-06-07 13:52:22 +0200

    fix elipsis, link twitter nicks

--- views/recent-posts/map.js
+++ views/recent-posts/map.js
@@ -1,7 +1,17 @@
 function(doc) {
   // only posts have slugs, dates, and html
   if (doc.type == "post") {
-    var summary = (doc.html.replace(/<(.|\n)*?>/g, '').substring(0,350) + '...');
+    // strip out tags for the summary
+    var summary = (doc.html.replace(/<(.|\n)*?>/g, '');
+    
+    // we only want a 350 char preview.
+    if(summary.length > 350) {
+      summary = summary.substring(0,350)) + "…";
+    }
+    
+    // link twitter names
+    // todo: make/use a library function
+    summary = summary.replace(/ @([^ .,]+)/g, ' <a href="http://twitter.com/$1">@$1</a>');
     emit(doc.created_at, {
       html : doc.html,
       summary : summary,
