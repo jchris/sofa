71769d815f64e3e1f5028e865bb3732fc3f95777
Chris Anderson <jchris@grabb.it>
2009-07-06 17:18:43 -0700

    validation cleanup

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -4,6 +4,7 @@ function (newDoc, oldDoc, userCtx) {
   unchanged("type");
   unchanged("author");
   unchanged("created_at");
+  
   if (newDoc.created_at) dateFormat("created_at");
 
   // docs with authors can only be saved by their author
@@ -16,27 +17,16 @@ function (newDoc, oldDoc, userCtx) {
   if (newDoc._deleted) return true;
     
   if (newDoc.type == 'post') {
-    require("created_at");
-    require("author");
-    require("body");
-    require("html");
-    require("format");
-    require("title");
-    require("slug");
-    require(newDoc.slug == newDoc._id, "Post slugs must be used as the _id.")
+    require("created_at", "author", "body", "html", "format", "title", "slug");
+    assert(newDoc.slug == newDoc._id, "Post slugs must be used as the _id.")
+
   } else if (newDoc.type == 'comment') {
-    // comment required fields
-    require("created_at");
-    require("post_id");
-    assert(newDoc.commenter && newDoc.commenter.name && newDoc.commenter.email, 
+    require("created_at", "post_id", "comment", "html", "format", "commenter");
+    assert(newDoc.commenter.name && newDoc.commenter.email, 
       "Comments must include name and email.");
-    require("html");
-    require("format");
-    require("comment");
-    if (newDoc.commenter && newDoc.commenter.url) {      
+    if (newDoc.commenter.url) {      
       assert(newDoc.commenter.url.match(/^https?:\/\/[^.]*\..*/), 
         "Commenter URL must start with http://.");      
     }
   }
-  return true;
 }
