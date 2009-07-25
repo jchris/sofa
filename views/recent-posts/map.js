e43da1928633aa855a848abb112db3098fbcba28
Chris Anderson <jchris@grabb.it>
2009-07-25 13:48:18 -0700

    drop comment

--- views/recent-posts/map.js
+++ views/recent-posts/map.js
@@ -1,5 +1,4 @@
 function(doc) {
-  // only posts have slugs, dates, and html
   if (doc.type == "post") {
     var summary = (doc.html.replace(/<(.|\n)*?>/g, '').substring(0,350) + '...');
     emit(doc.created_at, {
