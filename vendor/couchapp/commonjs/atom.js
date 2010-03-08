b2a9dbbb5d95750fdd8aceda332d5a7488d361de
Chris Anderson <jchris@apache.org>
2010-03-07 20:28:52 -0800

    got atom feeds working again

--- vendor/couchapp/commonjs/atom.js
+++ vendor/couchapp/commonjs/atom.js
@@ -1,3 +1,16 @@
+function f(n) {    // Format integers to have at least two digits.
+    return n < 10 ? '0' + n : n;
+}
+
+function rfc3339(date) {
+  return date.getUTCFullYear()   + '-' +
+    f(date.getUTCMonth() + 1) + '-' +
+    f(date.getUTCDate())      + 'T' +
+    f(date.getUTCHours())     + ':' +
+    f(date.getUTCMinutes())   + ':' +
+    f(date.getUTCSeconds())   + 'Z';
+};
+
 exports.header = function(data) {
   var f = <feed xmlns="http://www.w3.org/2005/Atom"/>;
   f.title = data.title;
@@ -5,7 +18,7 @@ exports.header = function(data) {
   f.link.@href = data.feed_link;
   f.link.@rel = "self";
   f.generator = "Sofa on CouchDB";
-  f.updated = data.updated.rfc3339();
+  f.updated = rfc3339(data.updated);
   return f.toXMLString().replace(/\<\/feed\>/,'');
 };
 
@@ -15,7 +28,7 @@ exports.entry = function(data) {
   entry.title = data.title;
   entry.content = data.content;
   entry.content.@type = (data.content_type || 'html');
-  entry.updated = data.updated.rfc3339();
+  entry.updated = rfc3339(data.updated);
   entry.author = <author><name>{data.author}</name></author>;
   entry.link.@href = data.alternate;
   entry.link.@rel = "alternate";
