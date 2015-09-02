8e9a5ec7c8d806cba678cadb59c4871280e845f9
Chris Hall <bitmonki@gmail.com>
2015-09-01 20:41:22 -1000

    ** Server-side changes (i.e., CouchDB-specific) *** lists/index.js:     Changed to return first element of key array rather than entire     array. *** views/lib/comments.js:     Apparently, from within maps, CouchDB will only include files from     the 'views/lib/' tree. Also changed to be more conformant to     CommonJS require standards used elsewhere in the app. (This was the     only macro invocation I could find in the entire project) *** views/lib/md5.js:     Added file so views/lib/comments.js could 'require' it. *** views/post-page/map.js:     Wasn't sorting properly, so nothing was being displayed, CouchDB     reported 'getRow() is null'.

--- views/post-page/map.js
+++ views/post-page/map.js
@@ -1,9 +1,9 @@
 function(doc) {
   var comments = require("views/lib/comments");
-  
+
   if (doc.type == "post") {
-    emit([doc._id], doc);
+    emit([doc._id, doc.created_at], doc);
   } else if (doc.type == "comment") {
     emit([doc.post_id, doc.created_at], comments.withGravatar(doc));
-  }
+  };
 };
